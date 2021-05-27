6af8a5cf02739c5af671326d219b522f0bfdd72a
okzapradhana <okzamahendra29@gmail.com>
2021-05-27 09:13:14 +0800

    fix path to use GCS on composer instead of use dags_folder path

--- dags/exercise_gcs_txt_to_bigquery_dag.py
+++ dags/exercise_gcs_txt_to_bigquery_dag.py
@@ -11,14 +11,16 @@ default_args = {
     'email': ['okzamahendra29@gmail.com'],
 }
 
-BASE_DIR = os.path.dirname(configuration.conf.get('core', 'dags_folder'))
+CONF_BASE_DIR = os.path.dirname(configuration.conf.get('core', 'dags_folder'))
+BASE_DIR = f"gs://{os.environ.get('BUCKET_NAME')}" if os.environ.get('ENVIRONMENT') == 'production' else CONF_BASE_DIR
+DATAFLOW_FUNCTIONS_PATH =  f'{BASE_DIR}/dataflow-functions/process_citizen_txt.py' if "gs://" in BASE_DIR else os.path.join(CONF_BASE_DIR, 'dataflow-functions', 'process_citizen_txt.py')
 
 @dag(default_args=default_args, schedule_interval='0 6 * * *', start_date=days_ago(1), tags=['dataflow-job'])
 def exercise_gcs_txt_to_bigquery_dag():
     txt_bq_dataflow_job = DataflowCreatePythonJobOperator(
         task_id='etl_txt_to_bq_dataflow_job',
         gcp_conn_id='google_cloud_default',
-        py_file=os.path.join(BASE_DIR, 'dataflow-functions', 'process_citizen_txt.py'),
+        py_file=DATAFLOW_FUNCTIONS_PATH,
         job_name='{{task.task_id}}',
         py_interpreter='python3',
         location='asia-south1'
--- dataflow-functions/process_citizen_txt.py
+++ dataflow-functions/process_citizen_txt.py
@@ -5,8 +5,10 @@ from airflow import configuration
 
 PROJECT_ID = "static-gravity-312212"
 DATASET_ID = "blankspace_de_dwh"
-TABLE_ID = "sample_citizen"
-BASE_DIR = os.path.dirname(configuration.conf.get('core', 'dags_folder'))
+TABLE_ID = "exercise_citizen_data" if os.environ.get("ENVIRONMENT") == "production" else "dev_exercise_citizen_data"
+TEMP_BUCKET_LOCATION = 'gs://blank-space-de-batch1-sg/tmp'
+CONF_BASE_DIR = os.path.dirname(configuration.conf.get('core', 'dags_folder'))
+BASE_DIR = f"gs://{os.environ.get('BUCKET_NAME')}" if os.environ.get('ENVIRONMENT') == 'production' else CONF_BASE_DIR
 
 class TxtTransformer(beam.DoFn):
   def __init__(self, delimiter):
@@ -27,10 +29,13 @@ class TxtTransformer(beam.DoFn):
     }]
 
 def run(argv=None):
+  print(f"Base Directory {BASE_DIR}")
   with beam.Pipeline() as pipeline:
-    rows = pipeline | "Read txt file" >> beam.io.ReadFromTextWithFilename(os.path.join(BASE_DIR, 'data', 'citizen.txt'))
+    file_path =  f'{BASE_DIR}/data/citizen.txt' if "gs://" in BASE_DIR else os.path.join(CONF_BASE_DIR, 'data', 'citizen.txt')
+    print(f"File source path {file_path}")
+    rows = pipeline | "Read txt file" >> beam.io.ReadFromTextWithFilename(file_path)
     pcoll_json = rows | "Transform txt to json/dict" >> beam.ParDo(TxtTransformer(','))
-    dict_records = pcoll_json | "Parse to BigQuery Rows" >> beam.Map(print) 
+    dict_records = pcoll_json | "Print P Collection Json Value" >> beam.Map(print) 
 
     bq_table_schema = {
       "fields": [
@@ -63,9 +68,9 @@ def run(argv=None):
     }
 
     pcoll_json | "Store data to BigQuery" >> beam.io.WriteToBigQuery(
-                          table=f'{PROJECT_ID}:{DATASET_ID}.{TABLE_ID}', #PROJECT_ID:DATASET_ID.TABLE
+                          table=f'{PROJECT_ID}:{DATASET_ID}.{TABLE_ID}',
                           schema=bq_table_schema,
-                          custom_gcs_temp_location='gs://blank-space-de-batch1-sg/temp',
+                          custom_gcs_temp_location=TEMP_BUCKET_LOCATION,
                           create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                           write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)
 
